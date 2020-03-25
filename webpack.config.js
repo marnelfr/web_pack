6fc1afe346c66a677d6a017bb0809a52e97e519c
Marnel <gmlginolias@gmail.com>
2020-03-25 09:30:14 +0100

    Adding TerserPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -37,5 +38,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new TerserPlugin()
+  ]
 }
